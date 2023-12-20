5fea50d2187f2e1785d998810c04e92257ccae9c
Liftpiloot <vandijkabel@gmail.com>
2023-12-20 13:36:00 +0100

    docs/api

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -7,7 +7,6 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserResource;
-use App\Models\Task;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
